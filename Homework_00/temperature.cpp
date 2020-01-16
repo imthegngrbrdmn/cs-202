f2afb8d211b052114a0ff2efdddd2da0d80b0645
maarnold7 <maarnold7@alaska.edu>
2020-01-16 11:09:27 -0900

    Working on testing temperature to make sure that command line options are working (argc & argv)

---
+++ Homework_00/temperature.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+double cpp_ftoc(const char* str);
+double c_ctof(const char* str);
+
+int main(int argc, const char** argv)
+{
+	if (argc >= 2 && argv[1] == std::string("--ftoc"))
+	{
+		cpp_ftoc(argv[2]);
+	}
+}
+double cpp_ftoc(const char* str)
+{
+	std::cout << "ftoc";
+	return 0;
+}
+double c_ctof(const char* str)
+{
+	std::cout << "ctof";
+	return 0;
+}
