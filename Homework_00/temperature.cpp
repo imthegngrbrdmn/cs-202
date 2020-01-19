7479a3d9454c4a44abedcc1b287f0d09bdd7cfc6
Millard Arnold V <millard.arnold.v@gmail.com>
2020-01-19 10:53:03 -0900

    Program successfully accepts command line arguments. Each option prints itself as being the selected option.

--- Homework_00/temperature.cpp
+++ Homework_00/temperature.cpp
@@ -5,9 +5,13 @@ double c_ctof(const char* str);
 
 int main(int argc, const char** argv)
 {
-	if (argc >= 2 && argv[1] == std::string("--ftoc"))
+	if (argc > 2 && argv[1] == std::string("--ftoc"))
 	{
-		cpp_ftoc(argv[2]);
+		cpp_ftoc(argv[3]);
+	}
+	if (argc > 2 && argv[1] == std::string("--ctof"))
+	{
+		c_ctof(argv[3]);
 	}
 }
 double cpp_ftoc(const char* str)
