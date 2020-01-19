aec436e53a8402ef8db701d94c2cd85a51d6816b
Millard Arnold V <millard.arnold.v@gmail.com>
2020-01-19 13:45:02 -0900

    Gutenberg program created. It prints an excerpt from The Celtic Twilight by W.B. Yeats.

---
+++ Homework_00/gutenberg.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int gutenberg()
+{
+	std::cout << "The host is riding from Knocknarea,"
+		<< "\nAnd over the grave of Clooth - na - bare;"
+		<< "\nCaolte tossing his burning hair,"
+		<< "\nAnd Niamh calling, \"Away, come away;"
+		<< "\nEmpty your heart of its mortal dream."
+		<< "\nThe winds awaken, the leaves whirl round,"
+		<< "\nOur cheeks are pale, our hair is unbound,"
+		<< "\nOur breasts are heaving, our eyes are a - gleam,"
+		<< "\nOur arms are waving, our lips are apart,"
+		<< "\nAnd if any gaze on our rushing band,"
+		<< "\nWe come between himand the deed of his hand,"
+		<< "\nWe come between himand the hope of his heart.\""
+		<< "\nThe host is rushing 'twixt night and day;"
+		<< "\nAnd where is there hope or deed as fair ?"
+		<< "\nCaolte tossing his burning hair,"
+		<< "\nAnd Niamh calling, \"Away, come away.\"";
+}
