9d0546b4d035635ce3091f9699ed545a26906ea2
Millard Arnold V <millard.arnold.v@gmail.com>
2020-01-19 12:01:19 -0900

    Handled input or output being so large that it overflows a double by printing an error message.

--- Homework_00/temperature.cpp
+++ Homework_00/temperature.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <limits>
 double cpp_ftoc(const char* str);
 double c_ctof(const char* str);
 
@@ -8,11 +9,21 @@ int main(int argc, const char** argv)
 	if (argc > 2 && argv[1] == std::string("--ftoc"))
 	{
 		double c=cpp_ftoc(argv[2]);
+		if (std::strtod(argv[2], NULL) > std::numeric_limits<double>::max() || c > std::numeric_limits<double>::max())
+		{
+			std::cout << "ERROR: Number too large";
+			return 0;
+		}
 		std::cout << std::strtod(argv[2], NULL) << " degrees Farenheit is " << c << " degrees Celsius.\n";
 	} 
 	else if (argc > 2 && argv[1] == std::string("--ctof"))
 	{
 		double f = c_ctof(argv[2]);
+		if (strtod(argv[2], NULL) > std::numeric_limits<double>::max() || f > std::numeric_limits<double>::max())
+		{
+			std::cout << "ERROR: Number too large";
+			return 0;
+		}
 		std::cout << strtod(argv[2], NULL) << " degrees Celsius is " << f << " degrees Farenheit.\n";
 	}
 	else
