6019f3a0d6f6570ed37db6878afe9794ff2e6cdb
maarnold7 <maarnold7@alaska.edu>
2020-01-23 11:11:05 -0900

    Created the jumpsuit class

--- Lab_02/classSource.cpp
+++ Lab_02/classSource.cpp
@@ -1,1 +1,28 @@
 #include "header.h"
+
+Jumpsuit::Jumpsuit()
+{
+	setColor(0xFF6600);
+}
+Jumpsuit::Jumpsuit(int color)
+{
+	setColor(color);
+	setOwner("Default");
+}
+Jumpsuit::Jumpsuit(const Jumpsuit& jump)
+{
+	setColor(jump.j_color);
+	setOwner(jump.owner);
+}
+void Jumpsuit::setOwner(std::string name)
+{
+	owner = name;
+}
+void Jumpsuit::setColor(int color)
+{
+	j_color = color;
+}
+Jumpsuit::~Jumpsuit()
+{
+	delete &owner;
+}
--- Lab_02/header.h
+++ Lab_02/header.h
@@ -2,6 +2,20 @@
 #define HEADER_H
 
 #include <iostream>
+#include <string>
 
+class Jumpsuit
+{
+private:
+	int j_color;
+	std::string owner;
+public:
+	Jumpsuit();
+	Jumpsuit(int color);
+	Jumpsuit(const Jumpsuit& jump);
+	virtual ~Jumpsuit();
+	void setColor(int color);
+	void setOwner(std::string name);
+};
 
 #endif
