2c458f7ef4101c3a8b2d8d5d459f41432f0baf08
Millard A. Arnold V <maarnold7@alaska.edu>
2020-01-23 18:59:24 -0900

    Added more parts of the main.

--- Lab_02/main.cpp
+++ Lab_02/main.cpp
@@ -8,8 +8,13 @@ int main(int argc, char** argv)
 	}
 	for (Jumpsuit suit : suits)
 	{
-		std::cout << suit.properties() << std::endl;
+		std::cout << "This is a jumpsuit with properites" << suit.properties() << std::endl;
 	}
 	Jumpsuit green = Jumpsuit(0x00FF00);
+	green.setOwner("Millard");
+	std::cout << "This is jumpsuit green \n" << green.properties() << std::endl;
+	Jumpsuit green2 = Jumpsuit(green);
+	std::cout << "This is jumpsuit green2\n" << green2.properties() << std::endl;
+
 	return 0;
 }
