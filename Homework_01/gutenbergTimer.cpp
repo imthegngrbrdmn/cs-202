a1f4b80765ce3c3c47494218fc52f171908f64a9
Millard A. Arnold V <maarnold7@alaska.edu>
2020-01-26 20:04:01 -0900

    gutenberg reads files

--- Homework_01/gutenbergTimer.cpp
+++ Homework_01/gutenbergTimer.cpp
@@ -1,5 +1,17 @@
 #include "stopwatch.h"
 int main(int argc, char** argv)
 {
-
+	StopWatch watch=StopWatch();
+	std::vector<std::string> titles={"alice.txt","cities.txt","eyre.txt","ion.txt","pride.txt","raven.txt"};
+	std::string line;
+	fstream book;
+	for(std::string title:titles)
+	{
+		std::vector<std::string> words;
+		book.open(title.c_str());
+		while(file>>word)
+		{
+			words.push_back(word);
+		}
+	}
 }
