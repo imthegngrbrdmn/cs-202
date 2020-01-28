ff86c69712179a719ed499068f5ab103b8a14435
maarnold7 <maarnold7@alaska.edu>
2020-01-28 11:01:07 -0900

    int pointers

--- Lab_03/main.cpp
+++ Lab_03/main.cpp
@@ -1,7 +1,12 @@
 #include<iostream>
 #include<string>
-
 int main(int argc, char** argv)
 {
-
+	int* intPtr = new int(5);
+	std::cout << "Made int* intPtr pointing to a 5: " << intPtr << std::endl;
+	std::unique_ptr<int> five=std::make_unique<int>(5);
+	std::cout << "Made unique_ptr<int> five pointing to a 5: " << five << std::endl;
+	std::unique_ptr<int> newFive = std::move(five);
+	std::cout << "Made unique_ptr<int> newFive which moved five: " << newFive << std::endl;
+	delete intPtr;
 }
