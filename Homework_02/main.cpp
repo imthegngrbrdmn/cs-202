e496cf459ea85c46aff24f23846268101ec20ba4
Millard Arnold V <millard.arnold.v@gmail.com>
2020-02-01 19:48:56 -0900

    Fixed queue test, made stack test

--- Homework_02/main.cpp
+++ Homework_02/main.cpp
@@ -7,16 +7,32 @@
 Value queue(std::list<Value> values, Value val)
 {
 	values.push_back(val);
+	Value retVal = values.front();
+	values.pop_front();
+	return retVal;
+}
+
+Value stack(std::list<Value> values, Value val)
+{
+	values.push_back(val);
 	Value retVal = values.back();
 	values.pop_back();
 	return retVal;
 }
+
 TEST_CASE("Queue is run", "[queue]")
 {
 	std::list<Value> values{ {5,"Five",'V'},{1,"One",'I'},{10,"Ten",'X'} };
 
-	REQUIRE(queue(values,{ 42,"TEST",'T' }) == Value({ 42, "TEST", 'T' }));
-	REQUIRE(queue(values,{ 0,"123",'\n' }) == Value({ 0, "123", '\n' }));
-	REQUIRE(queue(values,{ -7,"^%$@",'%' }) == Value({ -7, "^%$@", '%' }));
+	REQUIRE(queue(values, { 42,"TEST",'T' }) == Value({ 5,"Five",'V' }));
+	REQUIRE(queue(values, { 0,"123",'\n' }) == Value({ 5,"Five",'V' }));
+	REQUIRE(queue(values, { -7,"^%$@",'%' }) == Value({ 5,"Five",'V' }));
 }
+TEST_CASE("Stack is run", "[stack]")
+{
+	std::list<Value> values{ {5,"Five",'V'},{1,"One",'I'},{10,"Ten",'X'} };
 
+	REQUIRE(stack(values, { 42,"TEST",'T' }) == Value({ 42, "TEST", 'T' }));
+	REQUIRE(stack(values, { 0,"123",'\n' }) == Value({ 0, "123", '\n' }));
+	REQUIRE(stack(values, { -7,"^%$@",'%' }) == Value({ -7, "^%$@", '%' }));
+}
