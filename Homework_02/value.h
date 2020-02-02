29145bcd6b48f21b535982fd8a0d92e7a042f25e
Millard Arnold V <millard.arnold.v@gmail.com>
2020-02-01 20:19:59 -0900

    Added ostream << operator for Value

--- Homework_02/value.h
+++ Homework_02/value.h
@@ -2,16 +2,29 @@
 #define VALUE_H
 
 #include <string>
+#include <iostream>
 
 struct Value
 {
 	int integer;
 	std::string text;
 	char character;
+	bool operator<(const Value& a) const
+	{
+		return a.character < character;
+	}
+	bool operator>(const Value& a) const
+	{
+		return a.character > character;
+	}
 	bool operator==(const Value& a) const
 	{
 		return (a.character == character && a.integer == integer && a.text == text);
 	}
+	
 };
-
+std::ostream& operator<<(std::ostream& os, const Value& val)
+{
+	return os<<val.character<<"\t"<<val.integer<<"\t"<<val.text<<"\n";
+}
 #endif
