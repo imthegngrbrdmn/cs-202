438c8e0072080b4bb15b8bf53159c6507e8b5e8d
maarnold7 <maarnold7@alaska.edu>
2020-02-04 10:06:43 -0900

    Reads file. Writes input to file.

--- Lab_04/main.cpp
+++ Lab_04/main.cpp
@@ -4,5 +4,27 @@
 
 int main(int argc, char** argv)
 {
+	std::string line;
+	int n;
+	std::ifstream readFile ("text.txt");
+	if (readFile.is_open())
+	{
+		while (std::getline(readFile,line))
+		{
+			std::cout << line << '\n';
+		}
+	}
+	std::cout << "Enter a line of text.\n";
+	std::getline(std::cin, line);
+	std::cout << "Enter a number of times for that line to be appended: ";
+	std::cin >> n;
+
+	std::ofstream file;
+	file.open("text.txt", std::ios_base::app);
+	for (int i = 0; i < n; i++)
+	{
+		file << line << "\n";
+	}
+	file.close();
 	return 0;
 }
