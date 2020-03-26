be45a6ef0b70b9442c46e6587201db8a5c5cf6cc
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-26 18:17:50 -0400

    write factorial(n) using recursion

--- iditarod-anchorage/factorial.cpp
+++ iditarod-anchorage/factorial.cpp
@@ -8,3 +8,15 @@ int factorial_loop(int n)
 	}
 	return result;
 }
+
+int factorial(int n)
+{
+	if (n > 0)
+	{
+		return n * factorial(n - 1);
+	}
+	else if (n == 0)
+	{
+		return 1;
+	}
+}
--- iditarod-anchorage/factorial.h
+++ iditarod-anchorage/factorial.h
@@ -1,6 +1,5 @@
 #ifndef FACTORIAL_H
 #define FACTORIAL_H
-#include "../catch.hpp"
 
 int factorial_loop(int n);
 int factorial(int n);
