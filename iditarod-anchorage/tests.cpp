32bffe215a9208a5f490d62e5d4f9f5a278bdba1
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-26 18:10:26 -0400

    Use Catch2 to test factorial_loop(n)

--- iditarod-anchorage/tests.cpp
+++ iditarod-anchorage/tests.cpp
@@ -1,9 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
 #include "fib.h"
 #include "factorial.h"
-#include "../catch.hpp"
-#define CATCH_CONFIG_MAIN
 
-TEST_CASE("Factorials are computed", "[factorial]")
+TEST_CASE("Factorials are computed through a loop", "[factorial_loop]")
 {
-	
+	REQUIRE( factorial_loop(0) == 1 );
+	REQUIRE( factorial_loop(1) == 1 );
+	REQUIRE( factorial_loop(5) == 120 );
+	REQUIRE( factorial_loop(9) == 362880 );
 }
