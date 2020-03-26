7d701b0bb176e844d20fdf50870e1d4becc1f7d0
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-26 18:18:10 -0400

    Use Catch2 to test factorial(n)

--- iditarod-anchorage/tests.cpp
+++ iditarod-anchorage/tests.cpp
@@ -5,8 +5,16 @@
 
 TEST_CASE("Factorials are computed through a loop", "[factorial_loop]")
 {
-	REQUIRE( factorial_loop(0) == 1 );
-	REQUIRE( factorial_loop(1) == 1 );
-	REQUIRE( factorial_loop(5) == 120 );
-	REQUIRE( factorial_loop(9) == 362880 );
+	REQUIRE(factorial_loop(0) == 1);
+	REQUIRE(factorial_loop(1) == 1);
+	REQUIRE(factorial_loop(5) == 120);
+	REQUIRE(factorial_loop(9) == 362880);
+}
+
+TEST_CASE("Factorial are computed through recursion", "[factorial]")
+{
+	REQUIRE(factorial(0) == 1);
+	REQUIRE(factorial(1) == 1);
+	REQUIRE(factorial(5) == 120);
+	REQUIRE(factorial(9) == 362880);
 }
