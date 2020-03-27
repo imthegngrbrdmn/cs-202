c2bc57d1f1165b9f948ae1a18897ec54fc67eb84
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-26 20:03:16 -0400

    Write fib(n) through recursion

--- iditarod-anchorage/fib.cpp
+++ iditarod-anchorage/fib.cpp
@@ -21,3 +21,19 @@ int fib_loop(int n)
 	}
 	return sum;
 }
+
+int fib(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+	else if (n == 1)
+	{
+		return 1;
+	}
+	else if (n > 1)
+	{
+		return fib(n - 1) + fib(n - 2);
+	}
+}
