493c2c49c390fab7bfab89041e953502c38133a2
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-31 14:13:39 -0400

    Write ack(m,n)

--- iditarod-rainypass/ackermann.cpp
+++ iditarod-rainypass/ackermann.cpp
@@ -2,5 +2,17 @@
 
 int ack(int m, int n)
 {
-	return 0;
+	if (m == 0)
+	{
+		return n + 1;
+	}
+	else if (m > 0 && n == 0)
+	{
+		return ack(m - 1, 1);
+	}
+	else if (m > 0 && n > 0)
+	{
+		return ack(m - 1, ack(m, n - 1));
+	}
+	return -5;
 }
