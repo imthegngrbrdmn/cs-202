80d1fa3dc863ec6d7ea3281209467c3b6770cb94
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-03-31 14:13:48 -0400

    Test ack(m,n)

--- iditarod-rainypass/main.cpp
+++ iditarod-rainypass/main.cpp
@@ -1,6 +1,9 @@
 #include "ackermann.h"
+#include <iostream>
 
 int main(int argc, char** argv)
 {
+	std::cout << "ack(1, 1)\t"<<ack(1,1)<<std::endl;
+	std::cout << "ack(3, 5)\t"<<ack(3,5)<<std::endl;
 	return 0;
 }
