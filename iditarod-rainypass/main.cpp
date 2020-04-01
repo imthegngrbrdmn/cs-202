2dac6b2598af15777b6e5b79ac8e726e800fd937
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-01 14:59:02 -0400

    Time Largest Numbers

--- iditarod-rainypass/main.cpp
+++ iditarod-rainypass/main.cpp
@@ -4,13 +4,19 @@
 
 int main(int argc, char** argv)
 {
-	int m = 3, n = 9;	//m<4	n<10
+	//int m = 3, n = 9;	m<4	n<10
 	StopWatch watch;
 	double time;
 	int result;
-	watch.start();
-	result = ack(m, n);
-	time = watch.stop();
-	std::cout << "ack("<<m<<","<<n<<"):\t" << result << "\nTook:\t" << time << " seconds";
+	for (int m=3; m >= 0; m--)
+	{
+		for (int n=9; n >= 0; n--)
+		{
+			watch.start();
+			result = ack(m, n);
+			time = watch.stop();
+			std::cout << "ack(" << m << "," << n << "):\t" << result << "\nTook:\t" << time << " seconds\n\n";
+		}
+	}
 	return 0;
 }
