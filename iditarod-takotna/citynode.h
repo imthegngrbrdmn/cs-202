b35d9bfdc4cfc2138071242bb0696e20b3331a67
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-09 14:13:31 -0400

    Write Class CityNode

---
+++ iditarod-takotna/citynode.h
@@ -0,0 +1,21 @@
+#ifndef CITYNODE_H
+#define CITYNODE_H
+
+class CityNode
+{
+public:
+	CityNode();
+	~CityNode();
+
+private:
+
+};
+
+CityNode::CityNode()
+{
+}
+
+CityNode::~CityNode()
+{
+}
+#endif // !CITYNODE_H
