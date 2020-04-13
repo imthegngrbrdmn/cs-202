e432f79ad859eaf1306e30be7c03afc3ee529bbb
imthegngrbrdmn <maarnold7@alaska.edu>
2020-04-12 20:59:37 -0400

    Lab 11 initial commit

---
+++ Lab_011/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*
+[]	Default base class object
+[]	A base class object with one parameter constructor
+[]	Default derived class object
+[]	A derived class object with two parameter constructors
+*/
+
+int main(int argc, char** argv)
+{
+	return 0;
+}
