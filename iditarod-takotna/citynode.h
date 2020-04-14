3ec367d92f557867d801bd20891b43f1ba69b530
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-14 18:10:28 -0400

    Write getters & setters for CityNode

--- iditarod-takotna/citynode.h
+++ iditarod-takotna/citynode.h
@@ -3,6 +3,7 @@
 
 class CityNode
 {
+
 public:
 	CityNode();
 	~CityNode();
@@ -11,6 +12,13 @@ public:
 	double longitude()		{ return longitude_; }
 	double graphx()			{ return graphx_; }
 	double graphy()			{ return graphy_; }
+
+	bool setNumber(int num)			{ number_ = num; }
+	bool setLatitude(double lat)	{ latitude_ = lat; }
+	bool setLongitude(double longi)	{ longitude_ = longi; }
+	bool setgraphx(double x)		{ graphx_ = x; }
+	bool setgraphy(double y)		{ graphx_ = y; }
+
 private:
 	unsigned int number_;
 	double latitude_;
