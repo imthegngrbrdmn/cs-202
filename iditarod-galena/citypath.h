812cc763849d9c8bc1b25928b92f9d5e241ff64d
imthegngrbrdmn <imthegngrbrdmn@users.noreply.github.com>
2020-04-19 09:33:22 -0400

    Write path in CityPath

--- iditarod-galena/citypath.h
+++ iditarod-galena/citypath.h
@@ -6,9 +6,11 @@ class CityPath
 public:
 	CityPath()	{};
 	~CityPath() {};
+	void addToPath(int node)	{ path_.push_back(node); }
+	std::vector<int> path()		{ return path_; }
 
 private:
-
+	std::vector<int> path_;
 };
 
 #endif
